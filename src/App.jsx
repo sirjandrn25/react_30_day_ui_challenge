f416a775f212d8665fad961249821a03e6f05793
sirjan tamang <sirjantmg99@gmail.com>
2022-05-16 14:36:45 +0545

    day 15 complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
       <a target={"_blank"} href="day9-pomodoro" style={{"padding":"0px 20px"}} >Day9| Pomodoro</a> {" "}
       <a target={"_blank"} href="day12-image_gallery" style={{"padding":"0px 20px"}} >Day12| ImageGallery</a>
       <a target={"_blank"} href="day14-input-separator" style={{"padding":"0px 20px"}} >Day12| Input Separator</a>
+      <a target={"_blank"} href="day15-advice-generator" style={{"padding":"0px 20px"}} >Day12| Advice Generator</a>
     
     </div>
   )
