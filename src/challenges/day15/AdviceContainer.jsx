0ed767bc3eff985ecff7fab6733c4d4c6a7454e2
sirjan tamang <sirjantmg99@gmail.com>
2022-05-25 15:17:42 +0545

    advice ui complete

--- src/challenges/day15/AdviceContainer.jsx
+++ src/challenges/day15/AdviceContainer.jsx
@@ -20,32 +20,39 @@ const AdviceContainer = () => {
 
     React.useEffect(()=>{
         const response = fetchApi();
+        setToggle(true)
         response.then(data=>{
             setAdviceInfo(data)
+            setToggle(false)
         })
         document.title = "advice generator"
     },[]);
 
+
+    
+
     const refreshAdvice = ()=>{
+        setToggle(true)
         fetchApi().then(data=>{
             setAdviceInfo(data);
-            setToggle(!toggle);
+            setToggle(false);
+            
         })
         
     }
     
   return (
-    <div className='main_container'>
-        <div className="advice_box">
-            <div className="advice_no">
+    <div className='h-screen w-screen bg-sky-500 flex flex-row justify-center items-center'>
+        <div className="h-[609px] w-[1053px] rounded-[11px] bg-[#7879F1] flex flex-col justify-center items-center">
+            <div className="text-[32px] font-bold text-[#4BFF0C] mb-3">
                 Advice #{advice_info.id}
             </div>
 
-            <div className="advice_text">
+            <div className="font-bold text-[50px] w-[950px] text-center mb-6 text-white py-2 border-b-2">
              “{advice_info.advice}”
             </div>
-            <button onClick={refreshAdvice} className={`refresh_btn ${toggle?'active':''}`}>
-                <img src={refresh} alt="" />
+            <button onClick={refreshAdvice} className={` bg-white rounded-full ${toggle?'duration-500 rotate-[360deg]':''} p-4`}>
+                <img src={refresh} className={`h-[40px] w-[40px] object-cover` } alt="" />
             </button>
         </div>
       
