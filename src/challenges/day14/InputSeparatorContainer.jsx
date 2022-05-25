6818e9926e6bd14f6150b09521355d6c9a497196
sirjan tamang <sirjantmg99@gmail.com>
2022-05-25 13:20:15 +0545

    input separator done

--- src/challenges/day14/InputSeparatorContainer.jsx
+++ src/challenges/day14/InputSeparatorContainer.jsx
@@ -1,88 +1,106 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import CrossIcon from './cross.png'
 
+const bg_color_list = [
+    '#D21212','#7E12D2','#1276D2','#7E12D2','#00ff00','#ffff00','#999966','#660066'
+]
+const randomColor = ()=>{
+    const index = Math.floor(Math.random()*bg_color_list.length);
+    console.log(bg_color_list[index])
+
+    return bg_color_list[index]
+
+    
+}
 
 
 
 const InputSeparatorContainer = () => {
     const [items,setItems] = React.useState([]);
     
-    // const [error,setError] = React.useState('');
+    const [error,setError] = React.useState('');
     const [value,setValue] = React.useState('');
+   
     // const [invalid,setInvalid] = React.useState(false);
 
 
     const saveItem = ()=>{
-        // if(value.split('').indexOf('@')===-1 || value.split('').indexOf('.')===-1){
-        //     setError('please valid email address is fill')
+        let email_reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(!email_reg.test(value)){
+            setError('please valid email address is fill')
             
-        // }
-        // else{
-        //     setItems([value,...items])
-        //     setValue('')
-        //     setError('')
+        }
+        else{
+            const color = randomColor()
             
-        // }
-        setItems([value,...items])
-        setTimeout(()=>{
-            setValue('')
-        },100)
-
+            setItems([{content:value,color:`bg-[${color}]`},...items])
+            setTimeout(()=>{
+                setValue('');
+                setError('')
+            },100)
+            
+        }
         
+        
+       
         
     }
 
-    // const checkInvalidKey = ()=>{
-
-    //     // if(curr_key === ',' || curr_key === ' ' || curr_key.toLowerCase() === 'enter') return true 
-    //     if(curr_key === ' ') return true
-    //     return false
-    // }
 
     const handleKeyPress = e=>{
-        
+       
         if (e.key ===',' || e.key===' ' || e.key.toLowerCase() === 'enter'){
-            // setInvalid(true)
+        
+            let input = e.target;
+            
+            let end = input.value.length
+            
+            input.setSelectionRange(end,end)
             saveItem()
         }
-        // }else{
-        //     setInvalid(false);
-        // }
         
         
     }
-    const handleChange = e=>{
-        // console.log(invalid)
-        // if(!invalid){
-        //     setValue(e.target.value);
+    const removeEmptySpacesComma = stringVal => {
+        const regex = /(\s|\,)/g
+        return regex.test(stringVal);
+      };
     
-        // }
-        setValue(e.target.value);
-    }
+    const handleChange = e=>{
+        
+        const isValid = removeEmptySpacesComma(e.target.value)
+        if(!isValid){
+            setValue(e.target.value);
+        }
+        
 
+    }
     const handleDelete = (index)=>{
         let temp_items = items;
         items.splice(index,1)
         setItems([...temp_items])
     }
-    
   return (
-    <div className='main_container'>
-        <div className="wrapper">
-            <p className='header'>Enter a list  seperated by comma or enter to add items to list</p>
-            <div className="input_container">
-                <input type="email" name="email" className='input_control' onKeyPress={handleKeyPress} id="id_email" onChange={handleChange} value={value} placeholder='enter your text'  />
-                <button className="btn_submit" onClick={saveItem}>Submit →</button>
+    <div className='h-screen w-screen bg-[#5D5FEF] flex flex-row justify-center items-center'>
+        <div className="w-[1090px] h-[646px] rounded-[17px] bg-[#A5A6F6] flex flex-col items-center justify-center">
+            <p className='text-[#270343] text-[32px] font-bold mb-6'>Enter a list  seperated by comma or enter to add items to list</p>
+            <div className="input_container mb-6">
+                
+                    <input type="text" name="email"  className='w-[634px] h-[60px] rounded-[8px] border-4 focus:ring-blue-500 focus:border-blue-500 focus:outline-none px-3 ' onKeyPress={handleKeyPress} onChange={handleChange} id="id_email"  value={value} placeholder='email address'  />
+                    <button className="h-[60px] px-3 bg-[#1276D2] rounded-[8px] ml-[20px] text-[24px] text-white font-bold" onClick={saveItem}>Submit →</button>
+                
+                
             
             </div>
-            {/* {error?<p className='text_danger'>{error}</p>:''} */}
-            <div className="input_items" >
+            {error?<p className='text-red-500 font-bold text-[20px] mb-4'>{error}</p>:''}
+            <div className="flex flex-row justify-start max-w-[960px] flex-wrap" >
                 {items.map((item,index)=>{
                     return (
-                        <div className='item' key={index} >
-                            <span>{item}</span>
-                            <label onClick={e=>handleDelete(index)} className='icon_label'>
-                                <img src={CrossIcon} alt="" />
+                        <div className={`relative mb-4 ${item.color} mr-4 text-[20px] font-bold px-2 text-white rounded-[5px]`} key={index} >
+                            
+                            <span>{item.content} {item.color}</span>
+                            <label onClick={e=>handleDelete(index)} className='bg-[#EF775D] cursor-pointer absolute overflow-hidden -top-2 -right-2  rounded-full'>
+                                <img src={CrossIcon} alt="" className='h-full h-[18px] w-[18px] w-full object-cover' />
                             </label>
                         </div>
                     )
