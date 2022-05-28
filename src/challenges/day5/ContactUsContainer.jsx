9aaca66cdbc7fde0b205b8451ff67d80fef79c76
sirjan tamang <sirjantmg99@gmail.com>
2022-05-28 21:53:49 +0545

    day5 contactus success message

--- src/challenges/day5/ContactUsContainer.jsx
+++ src/challenges/day5/ContactUsContainer.jsx
@@ -8,29 +8,43 @@ const ContactUsContainer = () => {
     const [fullName,setFullName] = React.useState('');
     const [email,setEmail] = React.useState('');
     const [message,setMessage] = React.useState('');
+    const [submit,setSubmit] = React.useState(false);
 
     const handleSubmit = e=>{
         e.preventDefault();
         console.log(fullName)
         console.log(email)
         console.log(message)
+        setSubmit(true)
+    }
+    const renderleftContent = ()=>{
+        return (<div className='h-auto w-full px-5'>
+        <p className='font-bold capitalize text-[40px] md:text-[56px]'>Get In Touch</p>
+        <p className='text-[18px] md:text-[24px]'>We are help for you! How can we help you?</p>
+
+        <form action="" method='post' onSubmit={handleSubmit} className='mt-3'>
+            <input type="text" value={fullName} onChange={e=>setFullName(e.target.value)} name="fullName" placeholder='Full Name' className='w-full  h-[40px]  text-[15px] md:h-[56px] md:text-[20px] text-gray-600 rounded-md mb-3 px-3 focus:outline-none focus:border-2 focus:border-orange-300' />
+            <input type="email" value={email} onChange={e=>setEmail(e.target.value)} name="email" className='w-full h-[40px]  text-[15px] md:h-[56px] md:text-[20px] text-gray-600 rounded-md mb-3 px-3 focus:outline-none focus:border-2 focus:border-orange-300' placeholder='Email Address'/>
+            <textarea name="message" value={message} onChange={e=>setMessage(e.target.value)} className=' w-full h-[150px] text-[15px] md:text-[20px] text-gray-600 rounded-md mb-3 p-3 focus:outline-none focus:border-2 focus:border-orange-300' placeholder='your message'  ></textarea>
+            <button type='submit' className='bg-[#FF8E3C] py-2 w-full rounded-lg font-bold text-white'>Submit</button>
+        </form>
+    </div>)
+    }
+
+    const renderSuccessMessage = ()=>{
+        return (
+            <div>
+                <p className='text-[46px] md:text-[56px] font-bold'>Thanks</p>
+                <p className='text-[18px] md:text-[24px]'>We will get back to you shortly!</p>
+            </div>
+        )
     }
   return (
     <div className='h-auto md:h-screen w-screen flex justify-center items-center bg-gray-300 '>
         
         <div className=' h-screen  w-screen lg:w-[1000px] lg:h-[600px]  bg-[rgb(239,240,243)] md:bg-white border-0 grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-0'>
             <div className='h-full  bg-[rgb(239,240,243)] flex justify-center items-center'>
-                <div className='h-auto w-full px-5'>
-                    <p className='font-bold capitalize text-[40px] md:text-[56px]'>Get In Touch</p>
-                    <p className='text-[18px] md:text-[24px]'>We are help for you! How can we help you?</p>
-
-                    <form action="" onSubmit={handleSubmit} className='mt-3'>
-                        <input type="text" value={fullName} onChange={e=>setFullName(e.target.value)} name="fullName" placeholder='Full Name' className='w-full  h-[40px]  text-[15px] md:h-[56px] md:text-[20px] text-gray-600 rounded-md mb-3 px-3 focus:outline-none focus:border-2 focus:border-orange-300' />
-                        <input type="email" value={email} onChange={e=>setEmail(e.target.value)} name="email" className='w-full h-[40px]  text-[15px] md:h-[56px] md:text-[20px] text-gray-600 rounded-md mb-3 px-3 focus:outline-none focus:border-2 focus:border-orange-300' placeholder='Email Address'/>
-                        <textarea name="message" value={message} onChange={e=>setMessage(e.target.value)} className=' w-full h-[150px] text-[15px] md:text-[20px] text-gray-600 rounded-md mb-3 p-3 focus:outline-none focus:border-2 focus:border-orange-300' placeholder='your message'  ></textarea>
-                        <button type='submit' className='bg-[#FF8E3C] py-2 w-full rounded-lg font-bold text-white'>Submit</button>
-                    </form>
-                </div>
+                {submit?renderSuccessMessage():renderleftContent()}
             </div>
 
             <div className=' h-full flex flex-row justify-center items-center'>
