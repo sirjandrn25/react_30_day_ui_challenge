3a9198b0b41236b4fd71edf8f508d6adcaf34cdd
sirjan tamang <sirjantmg99@gmail.com>
2022-05-29 20:14:44 +0545

    fix live url

--- src/challenges/day14-week2/data.js
+++ src/challenges/day14-week2/data.js
@@ -1,74 +1,77 @@
-
-
-
 export const dataset = [
-    {
-        week:1,
-        'projects':[],
-    },
-    {
-        week:2,
-        'projects':[
-            {
-                'title':'pomodoro',
-                'day':10,
-                'thumbnail':'https://www.youtube.com/watch?v=Z9DTjlBm6mk&ab_channel=begintocode',
-                'tags':['starting out','buttons'],
-                'livelink':'https://imaginative-banoffee-db2159.netlify.app/day10-pomodoro',
-                'github':'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day10',
-                'figma':'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=635%3A403'
-            },
-            {
-                'title':'image gallery',
-                'day':12,
-                'thumbnail':'https://www.youtube.com/watch?v=5bT3SYz9-UE&ab_channel=begintocode',
-                'tags':['starting out','buttons'],
-                'livelink':'https://imaginative-banoffee-db2159.netlify.app/day12-image-gallery',
-                'github':'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day12',
-                'figma':'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=859%3A409'
-            },
-            
-            
-            {
-                'title':'Week 2 Project',
-                'day':14,
-                'thumbnail':'https://www.youtube.com/watch?v=gpWSlLlQChk&ab_channel=begintocode',
-                'tags':['starting out','buttons'],
-                'livelink':'https://imaginative-banoffee-db2159.netlify.app/day14-weekend-project',
-                'github':'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day14-week2',
-                'figma':'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=916%3A409'
-            },
-            
-           
-        ],
-    },
-    {
-        week:3,
-        projects:[
-            {
-                'title':'Input Separator',
-                'day':15,
-                'thumbnail':'https://www.youtube.com/watch?v=GqyDLWBtFA0&ab_channel=begintocode',
-                'tags':['starting out','buttons'],
-            
-                'livelink':'https://imaginative-banoffee-db2159.netlify.app/day15-input-separator',
-                'github':'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day15',
-                'figma':'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=996%3A409'
-            },
-            {
-                'title':'Advice generator',
-                'day':16,
-                'thumbnail':'https://www.youtube.com/watch?v=krIdSHUQVKw&ab_channel=begintocode',
-                'tags':['starting out','buttons'],
-                'livelink':'https://imaginative-banoffee-db2159.netlify.app/day16-advice-generator',
-                'github':'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day16',
-                'figma':'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=1029%3A409'
-            },
-        ]
-    },
-    {
-        week:4,
-        projects:[]
-    }
-    
+  {
+    week: 1,
+    projects: [
+      {
+        title: 'Buttons',
+        day: 1,
+        thumbnail: 'https://www.youtube.com/watch?v=Z9DTjlBm6mk&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day1-button',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day10',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=635%3A403',
+      },
+    ],
+  },
+  {
+    week: 2,
+    projects: [
+      {
+        title: 'pomodoro',
+        day: 10,
+        thumbnail: 'https://www.youtube.com/watch?v=Z9DTjlBm6mk&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day10-pomodoro',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day10',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=635%3A403',
+      },
+      {
+        title: 'image gallery',
+        day: 12,
+        thumbnail: 'https://www.youtube.com/watch?v=5bT3SYz9-UE&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day12-image-gallery',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day12',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=859%3A409',
+      },
+
+      {
+        title: 'Week 2 Project',
+        day: 14,
+        thumbnail: 'https://www.youtube.com/watch?v=gpWSlLlQChk&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day14-weekend-project',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day14-week2',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=916%3A409',
+      },
+    ],
+  },
+  {
+    week: 3,
+    projects: [
+      {
+        title: 'Input Separator',
+        day: 15,
+        thumbnail: 'https://www.youtube.com/watch?v=GqyDLWBtFA0&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day15-input-separator',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day15',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=996%3A409',
+      },
+      {
+        title: 'Advice generator',
+        day: 16,
+        thumbnail: 'https://www.youtube.com/watch?v=krIdSHUQVKw&ab_channel=begintocode',
+        tags: ['starting out', 'buttons'],
+        livelink: 'https://30day-ui-challenge-sirjantamang.netlify.app/day16-advice-generator',
+        github: 'https://github.com/sirjandrn25/react_30_day_ui_challenge/tree/main/src/challenges/day16',
+        figma: 'https://www.figma.com/file/QE89cpxxaOY3qbKMeIZ09Q/30Day?node-id=1029%3A409',
+      },
+    ],
+  },
+  {
+    week: 4,
+    projects: [],
+  },
 ]
