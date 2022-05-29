23e85d4aa1a53c268f6fdbc40b208027a3c311a4
sirjan tamang <sirjantmg99@gmail.com>
2022-05-29 12:19:59 +0545

    make responsive newsletter

--- src/challenges/day5/NewsLetterContainer.jsx
+++ src/challenges/day5/NewsLetterContainer.jsx
@@ -15,7 +15,7 @@ const NewsLetterContainer = () => {
 
     const renderModel = ()=>{
         return (
-            <div className='w-screen relative flex flex-col justify-center md:w-[600px] lg:w-[800px] h-[300px] md:h-[450px] rounded-[40px] shadow-lg bg-white border-2 border-white'>
+            <div className='w-screen relative flex flex-col justify-center items-center md:w-[600px] lg:w-[800px] h-auto sm:h-[450px] rounded-[40px] shadow-lg bg-white border-2 border-white'>
                 <img src={crossicon} alt="" onClick={handleOpen} className='px-5 absolute right-3 top-6 cursor-pointer h-[20px]' />
                     {success?<SubscribeSuccessContainer />:<SubscribeContainer error={error} validateEmail={validateEmail} />}
                     
@@ -59,15 +59,15 @@ const SubscribeContainer=(props)=>{
     }
     return (
         <>
-           <div className='w-full flex justify-center'>
-               <img src={open_mail} className='h-[120px] bg-sky-50 rounded-[45px] p-3 w-[120px]' alt="" />
-           </div>
-           <p className='font-bold text-center text-[44px] text-black/80'>Subscribe to our newsletter</p>
-           <p className='px-6 text-center text-[20px] text-gray-400'>Receive new articles and resources directly on your inbox. Fill your email below to join our email newsletter today.</p>
+           
+            <img src={open_mail} className='h-[90px] w-auto md:h-[120px] bg-sky-50 rounded-[45px] p-1 md:p-3 mt-6' alt="" />
+           
+           <p className='font-bold text-center text-[35px] md:text-[44px] text-black/80'>Subscribe to our newsletter</p>
+           <p className='px-6 text-center text-[15px] md:text-[22px] text-gray-400'>Receive new articles and resources directly on your inbox. Fill your email below to join our email newsletter today.</p>
 
-           <form action="" onSubmit={handleSubmit} className='w-full flex justify-center mt-4 relative items-center'>
-               <input type="text" value={email} onChange={e=>setEmail(e.target.value)} className='w-[680px] h-[70px] rounded-[71px] px-6 text-[20px] text-gray-700   shadow border border-1 outline-gray-200' placeholder='Enter your email address' />
-               <button type='submit' className='absolute right-[75px] bg-[#4A3AFF] py-3 px-4 rounded-[71px] text-white font-bold'>subscribe</button>
+           <form action="" onSubmit={handleSubmit} className='w-full flex justify-center mt-4 relative items-center px-8'>
+               <input type="text" value={email} onChange={e=>setEmail(e.target.value)} className='w-full h-[55px] md:h-[70px] rounded-[71px] px-6 text-[16px] md:text-[20px] text-gray-700   shadow border border-1 outline-gray-200' placeholder='Enter your email address' />
+               <button type='submit' className='absolute right-[50px] bg-[#4A3AFF] py-2 md:py-3 px-4 rounded-[71px] text-white font-bold'>subscribe</button>
            </form>
            {props.error?<p className='text-red-700 font-medium text-[18px] text-center'>{props.error}</p>:''}
         </>
